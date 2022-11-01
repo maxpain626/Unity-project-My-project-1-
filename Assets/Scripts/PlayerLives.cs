c1d8baafec025996b444a0397bdb5e3d3dc6b6dd
Max Maximov <46bublik@gmail.com>
2022-11-01 15:43:14 +0300

    Создал скрипт PlayerLives.cs, отслеживающий жизни героя

---
+++ Assets/Scripts/PlayerLives.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLives : MonoBehaviour
+{
+    void Start()
+    {
+        int playerLives = 3;
+
+        while (playerLives > 0)
+        {
+            Debug.Log("Still alive!");
+            playerLives--;
+        }
+        Debug.Log("Player KO'd...");
+    }
+
+    void Update()
+    {
+        
+    }
+}
