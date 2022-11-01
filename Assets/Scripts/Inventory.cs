4a875bc10c85dedc08fae6c20de3d474abb0fb7f
Max Maximov <46bublik@gmail.com>
2022-11-01 10:49:37 +0300

    Создал скрипт Inventory (словарь)

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    void Start()
+    {
+        Dictionary<string, int> itemInventory = new Dictionary<string, int>()
+        {
+            {"Potion", 5 },
+            {"Antidote", 7 },
+            {"Aspirin", 1 }
+        };
+        Debug.LogFormat("Items: {0}", itemInventory.Count);
+    }
+
+    void Update()
+    {
+        
+    }
+}
