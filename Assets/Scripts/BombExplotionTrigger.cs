1be3439111872d5da29ffaee3d8e2f022bb70968
Max Maximov <46bublik@gmail.com>
2022-11-08 13:31:27 +0300

    Добавил скрипт взаимодействия Player и Bomb

---
+++ Assets/Scripts/BombExplotionTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombExplotionTrigger : MonoBehaviour
+{
+    public GameObject triggerBomb;
+    public string nameAnimation;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(this.transform.gameObject);
+            Debug.Log("Boooom!");
+        }
+    }
+}
