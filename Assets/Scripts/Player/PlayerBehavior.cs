4c02299c30d1613dbdc30265f362bf4ccd4f3659
Max Maximov <46bublik@gmail.com>
2022-11-12 13:43:01 +0300

    Правки

--- Assets/Scripts/Player/PlayerBehavior.cs
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -6,17 +6,19 @@ public class PlayerBehavior : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float addForce = 5f;
+    public float distanceToGround = 0.1f;
+    public LayerMask groundLayer;
+
     private bool jumpHero = false;
     private Rigidbody2D body;
     private KeyCode addForceButton = KeyCode.Space;
-    private Vector2 direction;
-    private bool jump;
-
     private Animator animator;
+    private Collider2D collider2d;
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        collider2d = GetComponent<Collider2D>();
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -37,7 +39,7 @@ public class PlayerBehavior : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (Input.GetKey(addForceButton) && jumpHero)
+        if (Input.GetKey(addForceButton) && jumpHero == true)
         {
             body.velocity = new Vector2(0, addForce);
         }
