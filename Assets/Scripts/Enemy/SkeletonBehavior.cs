6be1917330e3db17506ed4f3a70675f88567535a
Max Maximov <46bublik@gmail.com>
2022-11-19 17:06:26 +0300

    Настройка рендеринга объектов + работа со скриптом SkeletonBehavior.cs

--- Assets/Scripts/Enemy/SkeletonBehavior.cs
+++ Assets/Scripts/Enemy/SkeletonBehavior.cs
@@ -10,13 +10,16 @@ public class SkeletonBehavior : MonoBehaviour
 
     void Start()
     {
-        GetComponent<BoxCollider2D>().offset = Vector2.Lerp(firstPosition, secondPosition, 1);
+        this.GetComponent<BoxCollider2D>().offset = new Vector2(0,0);
 
+        transform.position = new Vector2(0, 1);
+
+        transform.position += new Vector3(-1, 2, -1);
 
     }
 
     void Update()
     {
-        
+        transform.position += new Vector3(-0.1f, 0.6f, 0) * Time.deltaTime;
     }
 }
