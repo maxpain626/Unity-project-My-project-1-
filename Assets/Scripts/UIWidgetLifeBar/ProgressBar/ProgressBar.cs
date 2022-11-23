1f2e58ed086a2f3f0be7f02a249fa3b91a7ea7a6
Max Maximov <46bublik@gmail.com>
2022-11-23 13:46:39 +0300

    Настройка HealthBar и слоев

--- Assets/Scripts/UIWidgetLifeBar/ProgressBar/ProgressBar.cs
+++ Assets/Scripts/UIWidgetLifeBar/ProgressBar/ProgressBar.cs
@@ -5,12 +5,27 @@ using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
+    [Header("Health stats")]
     [SerializeField] private Image imgFiller;
+    [SerializeField] private float maxHealth = 100f;
+    private float currentHealth;
 
+    /*
     public void SetValue(float valueNormalized)
     {
         this.imgFiller.fillAmount = valueNormalized;
 
         var valueInPercent = Mathf.RoundToInt(f: valueNormalized * 100f);
+    }
+    */
+
+    public void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void Update()
+    {
+        
     }
 }
