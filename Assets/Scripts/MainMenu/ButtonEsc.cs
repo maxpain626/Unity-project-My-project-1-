22ffe09843460a7d1cac5c2752b6ccf65eef1f9b
Max Maximov <46bublik@gmail.com>
2022-11-25 05:28:57 +0300

    Сделал немног окрупнее кнопку ButtonEsc

---
+++ Assets/Scripts/MainMenu/ButtonEsc.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEsc : MonoBehaviour
+{
+    [SerializeField] private GameObject whatToOpenHide;
+
+    /*
+    private void OnClickOpenHide()
+    {
+        if (whatToOpenHide.active)
+        {
+
+        }
+    }
+    */
+}
