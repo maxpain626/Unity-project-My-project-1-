9a6e9d229bddef8a2ff7b626d3e2fe7f8ef899d4
Max Maximov <46bublik@gmail.com>
2022-12-21 18:14:53 +0300

    Работа с хп баром

---
+++ Assets/Scripts/Widgets/ProgressBar/HealthBar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
